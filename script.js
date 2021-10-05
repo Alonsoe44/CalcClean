12cfb53facec0fcbce3c8498557244a475da1027
Emiliano Ipad <emilianopolanco5@gmail.com>
2021-10-05 12:23:18 +0200

    Script changes

--- script.js
+++ script.js
@@ -2,8 +2,26 @@
 let display=0;
 let valueA= 0;
 let valueB= 0;
+let operator=0;
+let firtsValue = false;
 
 //Here the functions
-function resolve(){
-    dog
+function resolve(input){
+    valueA = input;
 }
+
+function operatorgetter(opType){
+		valueA = input;
+		operator = opType;
+		display = valueA;
+		firstValue = true;
+}
+
+function equalAction{
+	switch (operator) {
+		case(sum):
+		case()
+		
+	}
+}
+
