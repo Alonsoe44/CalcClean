e51c7916b6cea4b804fec3c39fb195e71c967afa
Emiliano Ipad <emilianopolanco5@gmail.com>
2021-10-06 02:12:38 +0200

    This is the day 2 Script advance

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 let display=0;
 let valueA= 0;
 let valueB= 0;
+let answer=0;
 let operator=0;
 let firtsValue = false;
 
@@ -10,17 +11,43 @@ function resolve(input){
     valueA = input;
 }
 
+//<<******Code for using the UI buttons*********>>
+
+//First Value setter
+
+function setValueA(inputUser){
+	//here i must place the inputUser in the display and make a string of numbers there
+}
+
 function operatorgetter(opType){
+		//I need to make a decision tree for the stupid user
+		//Transform string into number with decimal
 		valueA = input;
 		operator = opType;
 		display = valueA;
 		firstValue = true;
 }
 
-function equalAction{
+if(firstValue){
+	
+}
+
+function equalAction(){
 	switch (operator) {
-		case(sum):
-		case()
+		case 0:
+			answer = valueA+valueB;
+			break;
+		case 1:
+			answer = valueA-valueB;
+			break;
+		case 2:
+			answer = valueA*valueB;
+			break;
+		case 3:
+			answer = valueA/valueB;
+			break;
+		default:
+			
 		
 	}
 }
