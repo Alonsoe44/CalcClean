00a9db71a0654e188d2cee19b353355b9ffe4533
Apolbaq <emilianopolanco5@gmail.com>
2021-10-08 13:22:52 +0200

    This is a quick update day 5

--- scriptDom.js
+++ scriptDom.js
@@ -5,10 +5,15 @@ let screenInput = document.getElementById('theScreen')
 
 //Function on click
 
-n1.onclick = writeScreen('1');
-n2.onclick = writeScreen('2');
+n1.onclick = writeScreen;
+n2.onclick = writeScreen;
+
 
 function writeScreen(num){
     screenInput.value = screenInput.value+num;
     console.log(num);
 }
+
+//Quick note 
+//When using the buttons of the ui i need to cdhanges the input screen dir to write backwards
+//but it you type manually it should work like a normal calculator
